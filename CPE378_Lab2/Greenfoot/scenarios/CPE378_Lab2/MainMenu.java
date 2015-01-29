ed3aef20a9d53d960f99a8e3992bac0923febc67
gplaster <grant.plaster@yahoo.com>
2015-01-29 15:08:27 -0800

    Start Menu
    added new world for Start menu. Will change background image and
    button(s), but just wanted to get functionality started

---
+++ CPE378_Lab2/Greenfoot/scenarios/CPE378_Lab2/MainMenu.java
@@ -0,0 +1,36 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class MainMenu here.
+ * 
+ * @author Grant Plaster 
+ * @version 1-29-15
+ */
+public class MainMenu extends World
+{
+    StartButton start = new StartButton();
+    //InstructionButton instruction = new InstructionButton();
+    /**
+     * Constructor for objects of class MainMenu.
+     * 
+     */
+    public MainMenu()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1);
+        prepare();
+    }
+    
+    public void act() {
+        if (start.getStart() == true) {
+            Greenfoot.setWorld (new Lab2World());
+            Greenfoot.stop();
+        }
+    }
+    
+    public void prepare() {
+        getBackground().scale(1200, 1000);
+        addObject(start, 123, 72);
+        //addObject(instruction);
+    }
+}
---
+++ CPE378_Lab2/Greenfoot/scenarios/CPE378_Lab2/StartButton.java
@@ -0,0 +1,42 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class StartButton here.
+ * 
+ * @author Grant Plaster 
+ * @version 1-29-15
+ */
+public class StartButton extends Actor
+{
+    private boolean Startgame = false;
+        private boolean clicked = false;
+        private GreenfootImage background;
+    public StartButton() {
+
+        background = getImage();
+        updateImage();
+    }
+    /**
+     * Act - do whatever the StartButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if (Greenfoot.mouseClicked(this)) {
+            
+            Startgame = true;
+            updateImage();
+        }
+    }
+    
+    public boolean getStart() {
+        
+        return Startgame;
+    }
+    
+    private void updateImage() {
+        
+        GreenfootImage image = new GreenfootImage(background);
+        setImage(image);
+    }
+}
