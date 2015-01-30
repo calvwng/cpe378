060b11b8f72d51d0b9490f995382107e399fe605
DoctorPain99 <dr.pain99@gmail.com>
2015-01-29 17:46:56 -0800

    added basic scoring

--- CPE378_Lab2/Greenfoot/scenarios/CPE378_Lab2/PlayerBullet.java
+++ CPE378_Lab2/Greenfoot/scenarios/CPE378_Lab2/PlayerBullet.java
@@ -11,12 +11,16 @@ public class PlayerBullet extends Bullet {
     * Act - do whatever the PlayerBullet wants to do. This method is called whenever
     * the 'Act' or 'Run' button gets pressed in the environment.
     */
+   
+   public static int score = 0;
+   
    public void act() {
       Actor a = this.getOneIntersectingObject(Enemy.class);
       
       if (a != null) {
          getWorld().removeObject(a);
          getWorld().removeObject(this);
+         score += 100;
          return;
       }
       
