898907af2e02a3bcaee215f4afc7052a2dc75fca
DoctorPain99 <dr.pain99@gmail.com>
2015-02-05 15:55:45 -0800

    removed mover class
    moved the move method into the bullet class; it’s all we need

--- CPE378_Lab2/Greenfoot/scenarios/CPE378_Lab2/Bullet.java
+++ CPE378_Lab2/Greenfoot/scenarios/CPE378_Lab2/Bullet.java
@@ -6,7 +6,7 @@ import greenfoot.*;
  * @author Ian Anderson
  * @version Jan 25, 2015
  */
-public class Bullet extends Mover {
+public class Bullet extends Actor {
    public Bullet() {
       getImage().scale(12, 16);
       getImage().rotate(90);
@@ -22,5 +22,13 @@ public class Bullet extends Mover {
       if (getX() == 0 || getX() == getWorld().getWidth() - 1 || getY() == 0 || getY() == getWorld().getHeight() - 1) {
          getWorld().removeObject(this);
       }
-   }    
+   }
+   
+   public void move(double distance) {
+      double angle = Math.toRadians(getRotation());
+      int x = (int) Math.round(getX() + Math.cos(angle) * distance);
+      int y = (int) Math.round(getY() + Math.sin(angle) * distance);
+        
+      setLocation(x, y);
+   }
 }
