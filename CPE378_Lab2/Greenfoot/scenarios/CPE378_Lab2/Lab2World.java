e37b59cb95b29c2304f22f912fd6b722adc42814
calvwng <calvwng@gmail.com>
2015-02-05 05:13:45 -0800

    Added continuous spawning of enemies at random locs
    Currently 2 enemies spawn at random locations along the top and bottom
    edges of the screen every 1000 ms, or 1 second

--- CPE378_Lab2/Greenfoot/scenarios/CPE378_Lab2/Lab2World.java
+++ CPE378_Lab2/Greenfoot/scenarios/CPE378_Lab2/Lab2World.java
@@ -9,6 +9,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Lab2World extends GameWorld
 {
     static GreenfootSound bgMusic = new GreenfootSound("sounds/ufo.mp3");
+    final int ENEMY_SPAWN_INTERVAL = 1000;
+    double time = 0;
 
     /**
      * Constructor for objects of class Lab2World.
@@ -32,22 +34,43 @@ public class Lab2World extends GameWorld
         addObject(caption, getWidth()/2, getHeight() * 7 / 8);
         setCaption("Caption");
         toggleCaption(false);
+
+        //--- Set start time
+        time = System.currentTimeMillis();
     }
     
     public void populate() {
         Player player = new Player();
         addObject(player, getWidth()/2, getHeight()/2);
                 
-        //--- Spawn basic Enemies at random locations along border
-        for (int i = 0; i < 4; i++) {
-            int hgt = (i % 2 == 0) ? 1 : HEIGHT - 1;
-            addObject(new Enemy(), Greenfoot.getRandomNumber(WIDTH), hgt);    
-        }    
+        spawnEnemies(4);
 
         //--- Text
 
         //--- Scale the background image.
         getBackground().scale(708, 400);
+    }
+
+    /**
+    * Spawn |num| enemies at random locations along the top and bottom edge of the World
+    */
+    public void spawnEnemies(int num) {
+        //--- Spawn basic Enemies at random locations along border
+        for (int i = 0; i < num; i++) {
+            int rand = Greenfoot.getRandomNumber(2);
+            int hgt = (rand % 2 == 0) ? 1 : HEIGHT - 1;
+            addObject(new Enemy(), Greenfoot.getRandomNumber(WIDTH), hgt);    
+        }
+    }
+
+    public void act() {
+        double curTime = System.currentTimeMillis();
+
+        // Spawn some basic enemies for every interval completion
+        if (curTime - time >= ENEMY_SPAWN_INTERVAL) {
+            spawnEnemies(2);
+            time = curTime;
+        }
     }
 }
 
